f2f6674e7409cb65ec7b6967a1c23b11af548037
IFNMG <IFNMG@192.168.165.15>
2019-09-23 22:05:20 -0300

    test

---
+++ src/data.java
@@ -0,0 +1,16 @@
+
+public class data {
+    
+   
+public class Data 
+    String dia;
+    String mes;
+    String ano;
+    void imprimir ( ){
+        String data=this.dia+"/"this.mes+"/"+this.ano;
+    }
+            
+}
+
+  
+
